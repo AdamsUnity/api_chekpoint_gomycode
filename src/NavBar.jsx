da734471ec98470a1ccfde44c081ff7af5764339
Adams Unity <128307592+AdamsUnity@users.noreply.github.com>
2023-12-11 08:11:26 +0100

    firstcommit

---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+import { DingdingOutlined } from "@ant-design/icons";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="shadow-lg bg-blue-300 nav-header">
+      <div className="max-w-4xl mx-auto flex items-center gap-3">
+        <h3 className="text-gray-950">My_Product</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
